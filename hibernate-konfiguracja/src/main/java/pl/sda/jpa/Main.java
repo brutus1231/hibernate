ab789468a1b14d289bc8f7d265dee33451f9fcae
7g0r <40136311+7g0r@users.noreply.github.com>
2018-09-22 11:35:28 +0200

    Zadanie 1 zrealizowano

--- hibernate-konfiguracja/src/main/java/pl/sda/jpa/Main.java
+++ hibernate-konfiguracja/src/main/java/pl/sda/jpa/Main.java
@@ -1,8 +1,18 @@
 package pl.sda.jpa;
 
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Main {
     public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory =
+                Persistence.createEntityManagerFactory("manager1");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.close();
+        entityManagerFactory.close();
+
 
     }
 }
