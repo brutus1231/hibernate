b2ef038f08d7cf3cdd62b1f04b94dad6dba41e2e
Jakub Kus <jakkus@op.pl>
2018-09-22 11:45:13 +0200

    konfiguracja

--- hibernate-konfiguracja/src/main/java/pl/sda/jpa/Main.java
+++ hibernate-konfiguracja/src/main/java/pl/sda/jpa/Main.java
@@ -1,8 +1,16 @@
 package pl.sda.jpa;
 
 
-public class Main {
-    public static void main(String[] args) {
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
+public class Main {
+    public static void main (String[] args) {
+        EntityManagerFactory entityManagerFactory =
+                Persistence.createEntityManagerFactory("manager1");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.close();
+        entityManagerFactory.close();
     }
 }
