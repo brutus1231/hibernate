b849017bb7ef57dae164382ecca20f5ca9927851
pawelsokolowski <p.pawel.sokolowski@gmail.com>
2018-09-23 09:09:19 +0200

    ok

--- hibernate-encje/src/main/java/pl/sda/jpa/Main.java
+++ hibernate-encje/src/main/java/pl/sda/jpa/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
         DepartmentDaoImpl departamentDao = new DepartmentDaoImpl();
         departamentDao.create("IT2");
-
+        HibernateUtil.getInstance().close();
 
 
     }
